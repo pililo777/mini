98cf7c1647ef2e0547eb8118fd2058037d25e1dd
Ruben <pililo777@gmail.com>
2026-07-31 03:04:21 +0200

    Bump Mini SSH version for host fingerprint verification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.pililo777.minissh"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
     }
 }
 
